dc56a50939a5a57229826494c392b5d373fbf6a3
JustForFunDeveloper <tappler.andreas@gmail.com>
2019-05-09 21:40:25 +0200

    Add project files.

---
+++ GeoDoorServer3/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using GeoDoorServer3.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(GeoDoorServer3.Areas.Identity.IdentityHostingStartup))]
+namespace GeoDoorServer3.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<GeoDoorServer3Context>(options =>
+                    options.UseSqlite(
+                        context.Configuration.GetConnectionString("GeoDoorServer3ContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<GeoDoorServer3Context>();
+            });
+        }
+    }
+}
---
+++ GeoDoorServer3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GeoDoorServer3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
